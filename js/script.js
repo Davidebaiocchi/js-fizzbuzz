e1d3f0f718df3398497db842d7ded1f989d39a74
davide <davide.baiocchi97@gmail.com>
2021-06-04 10:05:50 +0200

    end with bonus

--- js/script.js
+++ js/script.js
@@ -4,13 +4,18 @@
 
 
 for(var i = 1; i <= 100; i++) {
-    if(i % 3 == 0 & i % 5 == 0) {
+    if(i % 3 == 0 & i % 5 == 0 & i % 6 == 0) {
+        console.log(i + 'FizzBuzzBoolean');
+    }else if(i % 3 == 0 & i % 5 == 0) {
         console.log(i + 'FizzBuzz');
     } else if(i % 5 == 0) {
         console.log(i + 'Buzz');
+    } else if(i % 6 == 0) {
+        console.log(i + 'FizzBoolean');
     } else if(i % 3 == 0) {
         console.log(i + 'Fizz');
     } else {
         console.log(i);
     }
 }
+
